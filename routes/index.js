b45837705971b82079a4b013254aa16569ccfff2
Leo Dalati <leo.dalati@ontariotechu.net>
2025-11-04 13:22:05 -0500

    changed layout to be in one page only and cleaned up folder

--- routes/index.js
+++ routes/index.js
@@ -6,24 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
-});
-
-/* GET AboutMe page. */
-router.get('/aboutMe', function(req, res, next) {
-  res.render('aboutMe', { title: 'About Me' });
-});
-
-/* GET projects page. */
-router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
-});
-
-/* GET contactMe page. */
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Me' });
-});
-
 module.exports = router;
